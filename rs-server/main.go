1b39f3ff6b5b3e0e592b925b0395af4c195f7610
Tailang-Li <litailang1994@gmail>
2019-06-19 18:21:38 +0800

    FEAT(conf/app.conf, conf/mysqlconf.go, main.go, routers/router.go): Add a service for providing the version of current service api

--- rs-server/main.go
+++ rs-server/main.go
@@ -1,1 +1,11 @@
-package rs_server
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"rs-engine/rs-server/controllers"
+)
+
+func main() {
+	beego.Router("/", &controllers.ApiController{}, "get:Version")
+	beego.Run()
+}
